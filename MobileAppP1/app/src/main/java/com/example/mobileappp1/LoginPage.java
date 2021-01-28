2eff7bbffaab8876903e7dd7aaf33cb915765b0b
MohamedAzarudeen <azartechnet@gmail.com>
2021-01-28 16:01:03 +0530

    28-01-2021

---
+++ MobileAppP1/app/src/main/java/com/example/mobileappp1/LoginPage.java
@@ -0,0 +1,44 @@
+package com.example.mobileappp1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class LoginPage extends Activity
+{
+    private Button b5;
+    private EditText e1,e2;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage);
+        e1=findViewById(R.id.editText);
+        e2=findViewById(R.id.editText2);
+        b5=findViewById(R.id.button4);
+        b5.setOnClickListener(new loginaction());
+    }
+
+    private class loginaction implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+            String t1=e1.getText().toString();
+
+            String t2=e2.getText().toString();
+
+            if(t1.equals("azar")&&t2.equals("azar@123"))
+            {
+                Toast.makeText(getApplicationContext(),"LoginSucess",Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"LoginFail",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ MobileAppP1/app/src/main/java/com/example/mobileappp1/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.mobileappp1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private Button b1,b2,b3;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.button);
+        b2=findViewById(R.id.button2);
+        b3=findViewById(R.id.button3);
+        b1.setOnClickListener(new loginpage());
+        b2.setOnClickListener(new adminpage());
+        b3.setOnClickListener(new registerpage());
+    }
+
+    private class loginpage implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+            Intent k1=new Intent(MainActivity.this,LoginPage.class);
+            startActivity(k1);
+            Toast.makeText(getApplicationContext(),"Your Click LoginButton",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class adminpage implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent k2=new Intent(MainActivity.this,AdminPage.class);
+            startActivity(k2);
+            Toast.makeText(getApplicationContext(),"Login AdminPage",Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private class registerpage implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent k3=new Intent(MainActivity.this,RegisterPage.class);
+            startActivity(k3);
+            Toast.makeText(getApplicationContext(),"RegisterPage",Toast.LENGTH_LONG).show();
+        }
+    }
+}
